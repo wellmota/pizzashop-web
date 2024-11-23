5d9d72afe85c4e25b645574eeedffff106f4197f
wellmota <impeto.wellington@gmail.com>
2024-11-23 01:05:05 -0300

    Initial commit for new project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+import "./App.css"
+
+export function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click me</Button>
+    </div>
+  )
+}
