58b7eb9d0c45198bb0df623a5280c53cf20e5341
wellmota <impeto.wellington@gmail.com>
2024-11-23 01:28:32 -0300

    Add ESLint and Prettier configuration files; update Tailwind CSS setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button"
-import "./App.css"
+import { Button } from "@/components/ui/button";
+import "./App.css";
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <Button>Click me</Button>
     </div>
-  )
+  );
 }
