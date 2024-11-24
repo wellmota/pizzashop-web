865560e62491981a57277f1c7543d0814b7beb29
wellmota <impeto.wellington@gmail.com>
2024-11-24 00:53:05 -0300

    Implement AppLayout and AuthLayout components; restructure routing for better layout management

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import { Dashboard } from "./pages/app/dashboard";
+// import { Signin } from "./pages/auth/signin";
+import { AppLayout } from "./pages/_layouts/app";
 import { Signin } from "./pages/auth/signin";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <Dashboard /> },
-  { path: "/sign-in", element: <Signin /> },
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [{ path: "/", element: <Dashboard /> }],
+  },
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [{ path: "/sign-in", element: <Signin /> }],
+  },
 ]);
