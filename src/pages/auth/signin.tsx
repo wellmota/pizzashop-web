bc15a05b32b10d3028b769a26b172d61843f1591
wellmota <impeto.wellington@gmail.com>
2024-11-26 16:06:11 -0300

    Add sonner for toast notifications; integrate Toaster in App component and update Signin component for success/error messages

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -3,6 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 import { z } from "zod";
+import { toast } from "sonner";
 
 import { useForm } from "react-hook-form";
 
@@ -20,8 +21,20 @@ export function Signin() {
   } = useForm<SignInForm>();
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    try {
+      console.log(data);
+
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+
+      toast.success("An email has been sent to you with the access link", {
+        action: {
+          label: "Reenviar",
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (error) {
+      toast.error("An error occured, please try again later");
+    }
   }
 
   return (
