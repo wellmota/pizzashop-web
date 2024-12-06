e15ae30162fd5407c9df027cf1ce8b4b05d52425
wellmota <impeto.wellington@gmail.com>
2024-12-06 13:23:18 -0300

    Add loading skeletons to AccountMenu component; implement API delay in axios configuration

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,4 +1,4 @@
-import { Building, ChevronDown, LogOut } from 'lucide-react';
+import { Building, ChevronDown, Divide, LogOut } from 'lucide-react';
 import { Button } from './ui/button';
 import {
   DropdownMenu,
@@ -11,17 +11,19 @@ import {
 import { getProfile } from '@/api/get-profile';
 import { useQuery } from '@tanstack/react-query';
 import { getManagedRestaurant } from '@/api/get-managed-restaurant';
+import { Skeleton } from './ui/skeleton';
 
 export function AccountMenu() {
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
   });
 
-  const { data: managedRestaurant } = useQuery({
-    queryKey: ['managed-restaurant'],
-    queryFn: getManagedRestaurant,
-  });
+  const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
+    useQuery({
+      queryKey: ['managed-restaurant'],
+      queryFn: getManagedRestaurant,
+    });
 
   return (
     <DropdownMenu>
@@ -30,16 +32,29 @@ export function AccountMenu() {
           variant="outline"
           className="flex item-center gap-2 select-none"
         >
-          {managedRestaurant?.name}
+          {isLoadingManagedRestaurant ? (
+            <Skeleton className="h-4 w-48" />
+          ) : (
+            managedRestaurant?.name
+          )}
           <ChevronDown className="h-4 w-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>{profile?.name}</span>
-          <span className="text-xs font-normal text-muted-foreground">
-            {profile?.email}
-          </span>
+          {isLoadingProfile ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-24" />
+              <Skeleton className="h-4 w-32" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-ts font-normal text-muted-foreground">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
