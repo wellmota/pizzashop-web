03ab18c75d25c36388a93387c731b445c6531e04
wellmota <impeto.wellington@gmail.com>
2025-01-23 12:27:20 -0300

    Add MSW setup for mocking API responses in test environment

---
+++ src/api/mocks/index.ts
@@ -0,0 +1,13 @@
+import { env } from '@/env';
+import { setupWorker } from 'msw/browser';
+import { signInMock } from './sign-in-mock';
+
+export const worker = setupWorker(signInMock);
+
+export async function enableMSW() {
+  if (env.MODE !== 'test') {
+    return;
+  }
+
+  await worker.start();
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './app.tsx';
+import { enableMSW } from './api/mocks/index.ts'
 
+enableMSW().then(()=>{
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+})
