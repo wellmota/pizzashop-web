4081bd87a1eb80e98fa36c073aa6a2150d813a78
wellmota <impeto.wellington@gmail.com>
2025-08-11 12:35:13 -0300

    fix: Vercel build imports (main.tsx) and tsconfig exclusions

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { App } from './app.tsx';
-import { enableMSW } from './api/mocks/index.ts'
+import { App } from './app';
+import { enableMSW } from './api/mocks';
 
 enableMSW().then(()=>{
 createRoot(document.getElementById('root')!).render(
