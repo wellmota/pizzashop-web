96efbe35e3018c957757acdd14b06cc155d257a6
wellmota <impeto.wellington@gmail.com>
2025-08-11 12:50:51 -0300

    fix(vercel): rename app.tsx to App.tsx, remove circular barrel export, use direct PascalCase import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { App } from './app/index';
+import { App } from './App';
 import { enableMSW } from './api/mocks';
 
 enableMSW().then(() => {
